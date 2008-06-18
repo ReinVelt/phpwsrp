e9ba77381440b53cc10df26ca5cf38de23ed325b
Rein.Velt <Rein.Velt@466aac24-ce4f-0410-831e-53ad69e408a4>
2008-06-18 13:58:14 +0000

    fixed comments

--- trunk/phpwsrp/wsrpcustomer.class.php
+++ trunk/phpwsrp/wsrpcustomer.class.php
@@ -5,7 +5,7 @@
 	* @description	This class provides all required methods to implement a wsrp porlet customer
 	*
 	* This source file is subject to the new BSD license that is bundled
-	* with this packae in the file LICENSE.txt. It is also available through
+	* with this package in the file LICENSE.txt. It is also available through
 	* the world-wide-web at this URL: http://framework.zend.com/license/new-bsd
 	* If you did not receive a copy of the license and are unable to obtain
 	* it through the world-wide-web, please send an email to rein@velt.org so
